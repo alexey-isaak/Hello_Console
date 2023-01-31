d4fdbdaf86e1f1184f8b8497ed1916dcb2f4f345
Alex Isaak <gafar@xaker.ru>
2023-01-31 20:20:55 +0300

    Enable .gitignore

---
+++ Hello_Console_2/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите ваше имя");
+        string username = Console.ReadLine();
+        Console.WriteLine($"Hi, ");
+        Console.WriteLine(username);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! 2");
+Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+
